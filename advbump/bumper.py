d2417ba0e30ff8a2f5dcd7388d2cea911a79f8b8
Hatworn <194559098+Hatworn@users.noreply.github.com>
2025-08-21 22:04:17 +0300

    Refactor code to ensure style consistency

--- advbump/bumper.py
+++ advbump/bumper.py
@@ -1,51 +1,51 @@
-from datetime import datetime
-import discord
 import asyncio
 import random
+from datetime import datetime
+
+import discord
+
 
 class Bumper:
-    def __init__(self, token: str, channel_id: int, delay: int,  bump_cooldown: int, max_random_delay: int):
-        self.token = token
-        self.channel_id = channel_id
-        
-        self.delay = delay
-        self.bump_cooldown = bump_cooldown
-        self.max_random_delay = max_random_delay
+    def __init__(self, token: str, channel_id: int, delay: int, bump_cooldown: int, max_random_delay: int) -> None:
+        self._token = token
+        self._channel_id = channel_id
 
-        self.client = discord.Client()
-        self.client.event(self.on_ready)
-    
-    async def run(self):
-        await self.client.start(self.token)
+        self._delay = delay
+        self._bump_cooldown = bump_cooldown
+        self._max_random_delay = max_random_delay
 
-    async def bump(self, channel_id: int):
+        self._client = discord.Client()
+        self._client.event(self.on_ready)
+
+    async def run(self) -> None:
+        await self._client.start(self._token)
+
+    async def on_ready(self) -> None:
+        await asyncio.sleep(self._delay)
+        await self._start_bumping()
+
+    async def _start_bumping(self) -> None:
+        while True:
+            await self._bump(self._channel_id)
+            await asyncio.sleep(self._bump_cooldown + random.uniform(1, self._max_random_delay))
+
+    async def _bump(self, channel_id: int) -> None:
         commands = []
-        channel = self.client.get_channel(channel_id)
+        channel = self._client.get_channel(channel_id)
         if not channel:
-            print(f'The bump channel of {channel.guild.name} is not found.')
+            print(f"The bump channel of {channel.guild.name} is not found.")
             return
-        
+
         application_commands = await channel.application_commands()
-        for cmd in application_commands:
-            if cmd.name == 'bump':
-                commands.append(cmd)
+        commands = [cmd for cmd in application_commands if cmd.name == "bump"]
 
         if not commands:
-            print(f'The bump channel of {channel.guild.name} has no bump command.')
+            print(f"The bump channel of {channel.guild.name} has no bump command.")
             return
-        
+
         for command in commands:
             await command.__call__(channel)
             await asyncio.sleep(2)
 
-        current_time = datetime.now().strftime('%d/%m/%y %H:%M:%S')
-        print(f'{current_time} - The server got bumped: {channel.guild.name}')
-
-    async def start_bumping(self):
-        while True:
-            await self.bump(self.channel_id)
-            await asyncio.sleep(self.bump_cooldown + random.uniform(1, self.max_random_delay))
-
-    async def on_ready(self):
-        await asyncio.sleep(self.delay)
-        await self.start_bumping()
+        current_time = datetime.now().strftime("%d/%m/%y %H:%M:%S")
+        print(f"{current_time} - The server got bumped: {channel.guild.name}")
--- advbump/main.py
+++ advbump/main.py
@@ -1,21 +1,24 @@
-import json
 import asyncio
+import json
+
 from bumper import Bumper
 
-CONFIG_PATH = 'config.json'
+CONFIG_PATH = "config.json"
 
-def load_config(path):
+
+def get_config(path: str) -> dict:
     with open(path) as file:
-        return(json.load(file))
-    
-def create_bumper_tasks(accounts, action_duration, bump_cooldown, max_random_delay):
+        return json.load(file)
+
+
+def create_bumper_tasks(accounts: list, action_duration: int, bump_cooldown: int, max_random_delay: int) -> list:
     tasks = []
     execution_delay = 0
     delay = action_duration / len(accounts)
 
     for account in accounts:
-        token = account['token']
-        channel_id = account['channel_id']
+        token = account["token"]
+        channel_id = account["channel_id"]
 
         bumper = Bumper(token, channel_id, execution_delay, bump_cooldown, max_random_delay)
         tasks.append(bumper.run())
@@ -23,16 +26,18 @@ def create_bumper_tasks(accounts, action_duration, bump_cooldown, max_random_del
 
     return tasks
 
-async def main():
-    config = load_config(CONFIG_PATH)
 
-    accounts = config['accounts']
-    action_duration = config['action_duration']
-    bump_cooldown = config['bump_cooldown']
-    max_random_delay = config['max_random_delay']
+async def main() -> None:
+    config = get_config(CONFIG_PATH)
+
+    accounts = config["accounts"]
+    action_duration = config["action_duration"]
+    bump_cooldown = config["bump_cooldown"]
+    max_random_delay = config["max_random_delay"]
 
     tasks = create_bumper_tasks(accounts, action_duration, bump_cooldown, max_random_delay)
     await asyncio.gather(*tasks)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
